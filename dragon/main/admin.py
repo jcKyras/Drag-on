d85c8ec0b738a8f5a8d36a358a0ac9fc5a9084e6
marcocspc <marcocspc@hotmail.com>
2019-12-17 16:36:51 -0300

    modelo impressoras adicionado

---
+++ dragon/main/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from main.models import Impressora
+from main.models import ModeloImpressora 
+
+# Register your models here.
+admin.site.register(Impressora)
+admin.site.register(ModeloImpressora)
---
+++ dragon/main/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Impressora(models.Model):
+
+    nome = models.CharField(max_length=50)
+    cont_impressoes = models.IntegerField()
+    numero_serie = models.CharField(max_length=255, primary_key=True)
+    modelo = models.ForeignKey(
+        'ModeloImpressora',
+        on_delete=models.CASCADE,
+    )
+    online = models.BooleanField()
+
+class ModeloImpressora(models.Model):
+
+    nome = models.CharField(max_length=255, primary_key=True)
