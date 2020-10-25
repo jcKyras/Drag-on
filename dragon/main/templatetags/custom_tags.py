bf43dc1a9f68016d3112f294c100013c56fcfd64
marcocspc <marcocspc@hotmail.com>
2020-10-24 21:11:13 -0300

    fiz o codigo de mostrar uma impressora basica, no entanto preciso criar um codigo para cadastrar uma impressora de exemplo, para nao precisar ficar recadastrando toda vida

--- dragon/main/templatetags/custom_tags.py
+++ dragon/main/templatetags/custom_tags.py
@@ -3,9 +3,17 @@ from django import template
 register = template.Library()
 
 @register.filter(name='porcentagem_impressora')
-def porcentagem_impresora(medicao):
+def porcentagem_impressora(medicao):
      impressora = medicao.impressora
 
      porcentagem = (impressora.cont_impressoes - medicao.contador)/impressora.cont_max_impressoes*100
 
      return str(porcentagem)
+
+@register.filter(name='calcula_contador_atual')
+def calcula_contador_atual(medicao):
+     impressora = medicao.impressora
+
+     contador = impressora.cont_impressoes - medicao.contador
+
+     return str(contador)
--- dragon/main/views.py
+++ dragon/main/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Medicao
+from .models import Medicao, Impressora
 
 # Create your views here.
 def home(request):
     
-    medicoes = Medicao.object.all()
+    impressoras = Impressora.objects.all()
+    ultimas_medicoes = []
+    for impressora in impressoras:
+        medicao = Medicao.objects.filter(impressora__nome=impressora.nome).latest()
+        ultimas_medicoes.append(medicao)
 
-    contexto = {'medicoes':medicoes}
+    contexto = {'ultimas_medicoes':ultimas_medicoes}
 
     return render(request, 'index.html', contexto)
